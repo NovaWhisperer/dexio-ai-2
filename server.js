2073932392a5ca13acac478efc9bfaf76e84f75b
Aryan Kaundal <aryankaundal2004@gmail.com>
2026-04-27 23:15:00 +0530

    feat: add uncaughtException and unhandledRejection handlers for improved error logging

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import app from "./src/app.js"
-
 import { PORT } from "./config/index.js"
 import { logger } from "./src/utils/logger.js"
 
-app.listen(PORT, () => {
-    logger.info("Server is listenning on PORT", {port: PORT})
+process.on("uncaughtException", (err, origin) => {
+    logger.error(err)
+    process.exit(1)
+
 })
+
+process.on("unhandledRejection", (reason, promise) => {
+    logger.error(reason)
+    process.exit(1)
+})
+
+app.listen(PORT, () => {
+    logger.info("Server is listenning on PORT", { port: PORT })
+})
+
