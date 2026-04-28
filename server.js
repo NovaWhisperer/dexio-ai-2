984444e80e3c4a493c2cd525bff430d310205e4c
Aryan Kaundal <aryankaundal2004@gmail.com>
2026-04-28 12:03:25 +0530

    feat: add mongoose dependency and implement database connection

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import app from "./src/app.js"
 import { PORT } from "./config/index.js"
 import { logger } from "./src/utils/logger.js"
+import connectDB from "./src/db/db.js"
 
 process.on("uncaughtException", (err, origin) => {
     logger.error(err)
     process.exit(1)
-
 })
 
 process.on("unhandledRejection", (reason, promise) => {
@@ -13,7 +13,14 @@ process.on("unhandledRejection", (reason, promise) => {
     process.exit(1)
 })
 
-app.listen(PORT, () => {
-    logger.info("Server is listenning on PORT", { port: PORT })
-})
+const startApplication = async (params) => {
+    await connectDB()
+
+    app.listen(PORT, () => {
+        logger.info("Server is listenning on PORT", { port: PORT })
+    })
+}
+
+startApplication()
+
 
---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "../../config/index.js";
+import { logger } from "../utils/logger.js";
+
+
+async function connectDB() {
+    try {
+        await mongoose.connect(MONGO_URI)
+        logger.info("Connected to DB")
+    } catch (error) {
+        logger.error(error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
