638b774b1e948d57453f92c6feb53125c2c1b379
Aryan Kaundal <aryankaundal2004@gmail.com>
2026-04-29 12:01:58 +0530

    feat: implement email verification process with token validation and expiry handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,9 +6,7 @@ import sendMailer from "../services/email.service.js"
 const registerController = async (req, res, next) => {
     const { fullName, email, password } = req.data
 
-    const emailExists = await userModel.findOne({
-        email
-    })
+    const emailExists = await userModel.findOne({ email })
 
     if (emailExists) {
         return res.status(409).json({
@@ -21,7 +19,6 @@ const registerController = async (req, res, next) => {
     const verificationToken = crypto.randomBytes(32).toString('hex');
 
     const verificationTokenExpiry = new Date().getTime() + 24 * 60 * 60 * 1000
-
 
     try {
         const user = await userModel.create({
@@ -46,15 +43,47 @@ const registerController = async (req, res, next) => {
     }
 }
 
+const verifyEmailController = async (req, res, next) => {
+
+    try {
+        const { token } = req.query;
+        const user = await userModel.findOne({ verificationToken: token })
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Invalid Token"
+            })
+        }
+
+        if (user.verified === true) {
+            return res.status(200).json({
+                message: "User already verified"
+            })
+        }
+
+        if (user.verificationTokenExpiry > (new Date().getTime())) {
+
+            await userModel.findOneAndUpdate({ verificationToken: token }, { $set: { verified: true } })
+
+            return res.status(200).json({
+                message: "User email verified successfully"
+            })
+        }
+        else {  
+            return res.status(400).json({
+                message:"Token had expired. Register again"
+            })
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
 const loginController = (req, res) => {
 
 }
 
 const logoutController = (req, res) => {
-
-}
-
-const verifyEmailController = (req, res) => {
 
 }
 
@@ -73,7 +102,6 @@ const forgotPasswordController = (req, res) => {
 const resetPasswordController = (req, res) => {
 
 }
-
 
 
 export { registerController, loginController, logoutController, verifyEmailController, googleController, googleCallbackController, forgotPasswordController, resetPasswordController }
